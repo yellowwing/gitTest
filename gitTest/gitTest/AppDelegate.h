1dfe4f450b2b7c5884dfc8233c944517efe5c948
yellowwing <923133828@qq.com>
2021-01-07 10:10:03 +0800

    初始化项目

---
+++ gitTest/gitTest/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  gitTest
+//
+//  Created by yellow on 2021/1/7.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ gitTest/gitTest/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  gitTest
+//
+//  Created by yellow on 2021/1/7.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
